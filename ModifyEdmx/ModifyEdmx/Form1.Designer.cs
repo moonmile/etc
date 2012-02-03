9fa025b111985ad84d0c34fe927ea73f7b85be44
moonmile <masuda@moonmile.net>
2012-02-03 11:55:44 +0900

    Edmxファイルの編集ツール
    LINQで扱う時に、テーブル名/カラム名を UpperCaml にするとか。

---
+++ ModifyEdmx/ModifyEdmx/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace ModifyEdmx
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.button2 = new System.Windows.Forms.Button();
+			this.textTableName = new System.Windows.Forms.TextBox();
+			this.buttonTableUpdate = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(13, 13);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(99, 42);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "読込";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 12;
+			this.listBox1.Location = new System.Drawing.Point(14, 62);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(141, 268);
+			this.listBox1.TabIndex = 1;
+			this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AllowUserToAddRows = false;
+			this.dataGridView1.AllowUserToDeleteRows = false;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+			this.dataGridView1.Location = new System.Drawing.Point(161, 62);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowTemplate.Height = 21;
+			this.dataGridView1.Size = new System.Drawing.Size(364, 318);
+			this.dataGridView1.TabIndex = 2;
+			this.dataGridView1.CellBeginEdit += new System.Windows.Forms.DataGridViewCellCancelEventHandler(this.dataGridView1_CellBeginEdit);
+			this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(118, 13);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(99, 42);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "保存";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// textTableName
+			// 
+			this.textTableName.Location = new System.Drawing.Point(14, 337);
+			this.textTableName.Name = "textTableName";
+			this.textTableName.Size = new System.Drawing.Size(100, 19);
+			this.textTableName.TabIndex = 6;
+			// 
+			// buttonTableUpdate
+			// 
+			this.buttonTableUpdate.Location = new System.Drawing.Point(121, 335);
+			this.buttonTableUpdate.Name = "buttonTableUpdate";
+			this.buttonTableUpdate.Size = new System.Drawing.Size(34, 23);
+			this.buttonTableUpdate.TabIndex = 7;
+			this.buttonTableUpdate.Text = "更";
+			this.buttonTableUpdate.UseVisualStyleBackColor = true;
+			this.buttonTableUpdate.Click += new System.EventHandler(this.buttonTableUpdate_Click);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(427, 14);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(99, 42);
+			this.button4.TabIndex = 8;
+			this.button4.Text = "一律変更";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// Column1
+			// 
+			this.Column1.DataPropertyName = "ColumnName";
+			this.Column1.HeaderText = "DB名";
+			this.Column1.Name = "Column1";
+			this.Column1.ReadOnly = true;
+			// 
+			// Column2
+			// 
+			this.Column2.DataPropertyName = "Name";
+			this.Column2.HeaderText = "マッピング名";
+			this.Column2.Name = "Column2";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(538, 393);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.buttonTableUpdate);
+			this.Controls.Add(this.textTableName);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Modify Edmx ver.0.1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox textTableName;
+		private System.Windows.Forms.Button buttonTableUpdate;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+	}
+}
+
---
+++ ModifyEdmx/ModifyEdmx/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using moonmile.ExDoc;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace ModifyEdmx
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			dataGridView1.AutoGenerateColumns = false;
+		}
+
+		string _filename = @"Model1.edmx";
+		EXDocument _doc = null;
+		List<TableMapping> _tables;
+		List<ScalarProperty> _columns;
+		string _modelName;
+
+		/// <summary>
+		/// 読込ボタン
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void button1_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog dlg = new OpenFileDialog();
+			dlg.Filter = "edmx files (*.edmx)|*.edmx|All files (*.*)|*.*";
+			dlg.FilterIndex = 0;
+			if (dlg.ShowDialog() != DialogResult.OK)
+				return;
+
+			_filename = dlg.FileName;
+			_doc = new EXDocument();
+			_doc.Load(_filename);
+
+			_tables = new List<TableMapping>();
+			var els = _doc * "edmx:Mappings" * "EntitySetMapping";
+			foreach (var el in els)
+			{
+				TableMapping tm = new TableMapping();
+				_tables.Add(tm);
+				tm.EntityName = el / "EntityTypeMapping" % "TypeName";
+				tm.EntityName = tm.EntityName.Substring(tm.EntityName.IndexOf('.') + 1);
+				tm.EntitySetName = el % "Name";
+				tm.StoreName = el / "EntityTypeMapping" / "MappingFragment" % "StoreEntitySet";
+			}
+			listBox1.DataSource = _tables;
+
+			EXElement el2 = _doc * "EntitySet";
+			string name = el2 % "EntityType";
+			_modelName = name.Substring(0, name.IndexOf('.') );
+		}
+
+		/// <summary>
+		/// リスト選択
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if (listBox1.SelectedIndex == -1) return;
+			TableMapping tm = (TableMapping)listBox1.SelectedItem;
+			string name = tm.EntityName;
+			EXElement el = _doc * "MappingFragment" % "StoreEntitySet" == tm.StoreName;
+			var items = from t in el / "ScalarProperty"
+						select new ScalarProperty
+						{
+							Name = t % "Name",
+							ColumnName = t % "ColumnName"
+						};
+			_columns = items.ToList();
+			dataGridView1.DataSource = _columns;
+
+			textTableName.Text = name;
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			SaveFileDialog dlg = new SaveFileDialog();
+			dlg.DefaultExt = "emdx";
+			dlg.Filter = "edmx files (*.edmx)|*.edmx|All files (*.*)|*.*";
+			dlg.FilterIndex = 0;
+			if (dlg.ShowDialog() != DialogResult.OK)
+				return;
+
+			string filename = dlg.FileName;
+			_doc.Save( filename);
+			MessageBox.Show("保存しました");
+		}
+
+		/// <summary>
+		/// テーブル名を更新
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void buttonTableUpdate_Click(object sender, EventArgs e)
+		{
+			int idx = listBox1.SelectedIndex;
+			string entityNameOld = _tables[idx].EntityName;
+			string entityTypeOld = _modelName + "." + entityNameOld;
+			string entityNameNew = textTableName.Text;
+			string entityTypeNew = _modelName + "." + entityNameNew;
+			EXElement el = _doc * "edmx:ConceptualModels" * "EntitySet" % "EntityType" == entityTypeOld;
+			el["Name"] = entityNameNew;
+			el["EntityType"] = entityTypeNew;
+
+
+			el = _doc * "edmx:ConceptualModels" * "EntitySet" % "EntityType" == entityTypeOld;
+			el["Name"] = entityNameNew;
+			el["EntityType"] = entityTypeNew;
+			el = _doc * "edmx:ConceptualModels" * "EntityType" % "Name" == entityNameOld;
+			el["Name"] = entityNameNew;
+
+			el = _doc * "edmx:Mappings" * "EntityTypeMapping" % "TypeName" == entityTypeOld;
+			el["TypeName"] = entityTypeNew;
+			el.Parent["Name"] = entityNameNew;
+
+
+
+			el = _doc * "Diagrams" * "EntityTypeShape" % "EntityType" == entityTypeOld;
+			el["EntityType"] = entityTypeNew;
+
+			_tables[idx].EntityName = entityNameNew;
+			_tables[idx].EntitySetName = entityNameNew;
+
+			listBox1.DataSource = null;
+			listBox1.DataSource = _tables;
+			listBox1.SelectedIndex = idx;
+
+		}
+
+		private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+		{
+			int column = e.ColumnIndex;
+			int row = e.RowIndex;
+			if (column < 0 || row < 0)
+				return;
+
+			var cell = dataGridView1.Rows[row].Cells[column];
+			string val = (string)cell.Value;
+			Debug.Print("{0},{1} {2} {3}", row, column, _oldProperty.ColumnName, val);
+
+			// 該当箇所を変更
+			ChangeScalarProp(_oldProperty.Name, val);
+		}
+
+		/// <summary>
+		/// カラム名を変更
+		/// </summary>
+		/// <param name="oldName"></param>
+		/// <param name="newName"></param>
+		private void ChangeScalarProp( string oldName, string newName)
+		{
+			string entityName = _tables[listBox1.SelectedIndex].EntityName;
+			EXElement el = _doc * "edmx:ConceptualModels" * "EntityType" % "Name" == entityName;
+
+			EXElement elref = el * "PropertyRef" % "Name" == oldName;
+			if (EXDocument.IsEmpty(elref) == false)
+			{
+				elref["Name"] = newName;
+			}
+			EXElement elprop = el * "Property" % "Name" == oldName;
+			elprop["Name"] = newName;
+
+			string storeName = _tables[listBox1.SelectedIndex].StoreName;
+			el = _doc * "edmx:Mappings" * "MappingFragment" % "StoreEntitySet" == storeName;
+			elprop = el / "ScalarProperty" % "Name" == oldName;
+			elprop["Name"] = newName;
+		}
+
+		/// <summary>
+		/// 編集前の情報を保持
+		/// </summary>
+		ScalarProperty _oldProperty;
+		/// <summary>
+		/// 編集前の情報を保持する
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void dataGridView1_CellBeginEdit(object sender, DataGridViewCellCancelEventArgs e)
+		{
+			int column = e.ColumnIndex;
+			int row = e.RowIndex;
+			if (column < 0 || row < 0)
+				return;
+
+			_oldProperty = new ScalarProperty()
+			{
+				Name = _columns[row].Name,
+				ColumnName = _columns[row].ColumnName
+			};
+		}
+
+		/// <summary>
+		/// 列名を一律で変更
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void button4_Click(object sender, EventArgs e)
+		{
+			foreach (ScalarProperty prop in _columns)
+			{
+				string oldName = prop.Name;
+				string newName = oldName;
+				newName = newName.Substring(0, 1).ToUpper() + newName.Substring(1);
+				while (true)
+				{
+					int i = newName.IndexOf('_');
+					if (i < 0) break;
+					newName = newName.Substring(0, i ) +
+						newName.Substring(i + 1, 1).ToUpper() +
+						newName.Substring(i + 2);
+				}
+				ChangeScalarProp(oldName, newName);
+				prop.Name = newName;
+			}
+			dataGridView1.DataSource = null;
+			dataGridView1.DataSource = _columns;
+		}
+	}
+
+
+	/// <summary>
+	/// テーブルマッピング
+	/// </summary>
+	public class TableMapping
+	{
+		public string StoreName { get; set; }		// 元テーブルの名前
+		public string EntityName { get; set; }		// エンティティの名前
+		public string EntitySetName { get; set; }	// エンティティセットの名前
+		// 表示用
+		public override string ToString()
+		{
+			return string.Format("{0}({1})", EntityName, StoreName);
+		}
+	}
+
+
+	/// <summary>
+	/// カラム名マッピングクラス
+	/// </summary>
+	public class ScalarProperty
+	{
+		public string Name { get; set; }
+		public string ColumnName { get; set; }
+	}
+}
